d0cda774e90969a43500dd1d752e42ebfdf31699
Stephen Kane <steve.kane@live.com>
2015-01-21 23:11:09 -0500

    added files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# pi-remote
+# Raspberry Pi Remote Control
+
+## Sources:
+
+* http://askubuntu.com/questions/492361/are-there-commands-to-control-the-spotify-for-linux-preview-e-g-pause
+* http://specifications.freedesktop.org/mpris-spec/latest/Player_Interface.html
+* http://alexba.in/blog/2013/06/08/open-source-universal-remote-parts-and-pictures/
+* http://alexba.in/blog/2013/01/06/setting-up-lirc-on-the-raspberrypi/
+* https://pypi.python.org/pypi/python-lirc/1.2.1
+* http://www.xantech.com/files/manuals/127_39020_manual.pdf
+
+## Items
+
+* raspberry pi
+* IR sensor from Comcast digital converter (CTI#98-IRB-6)
---
+++ remote.py
@@ -0,0 +1,91 @@
+import lirc, sys
+import subprocess
+import re
+
+BUS_SPOTIFY="qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player."
+HOST="skane@192.168.1.9"
+sessionkey=""
+
+def main():
+    sockid = lirc.init("lircremote", blocking = True)
+    retrieve_session_key()
+    while True:
+        codeIR = lirc.nextcode()
+
+        if codeIR:
+            exec(codeIR[0] + '()')
+
+
+# *************************************** #
+#             SSH
+# *************************************** #
+def simple_ssh(command, handler):
+    return my_ssh(command, handler, ["ssh", "%s" % HOST, command])
+
+def session_ssh(command, handler):
+    return my_ssh(command, handler, ["ssh", "%s" % HOST, "DBUS_SESSION_BUS_ADDRESS=\""+sessionkey+"\"", command])
+
+def my_ssh(command, handler, popen_arg):
+    ssh = subprocess.Popen(popen_arg, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    result = ssh.stdout.readlines()
+    if result == []:
+        error = ssh.stderr.readlines()
+        print >>sys.stderr, "ERROR: %s" % error
+    else:
+        if handler:
+            return handler(result)
+        return result
+
+
+# *************************************** #
+#             BUS SESSION
+# *************************************** #
+def retrieve_session_key():
+    global sessionkey
+    sessionkey = simple_ssh("cat ~/spotifySessionFile", False)[0].strip()
+
+
+# *************************************** #
+#               VOLUME
+# *************************************** #
+def volume_change_handler(result):
+    print re.search('\[\d*%\]', result[5].strip()).group(0)
+def volume_up():
+    simple_ssh("amixer -D pulse sset Master 5%+", volume_change_handler)
+def volume_down():
+    simple_ssh("amixer -D pulse sset Master 5%-", volume_change_handler)
+
+
+# *************************************** #
+#               SPOTIFY
+# *************************************** #
+def spotify_callback_handler(result):
+    print result
+
+def spotify_playpause():
+    session_ssh(BUS_SPOTIFY + "PlayPause", spotify_callback_handler)
+def spotify_next():
+    session_ssh(BUS_SPOTIFY + "Next", spotify_callback_handler)
+def spotify_previous():
+    session_ssh(BUS_SPOTIFY + "Previous", spotify_callback_handler)
+def spotify_status():
+    session_ssh(BUS_SPOTIFY + "PlaybackStatus", spotify_callback_handler)
+
+
+# *************************************** #
+#               COMMANDS
+# *************************************** #
+def close_remote():
+    session_ssh("notify-send 'closing remote'",False)
+    print "Shutting down remote."
+    sys.exit()
+
+def check_it():
+    exec('close_remote()')
+
+def not_supported():
+    print "Not supported."
+    #session_ssh("notify-send 'invalid entry'",False)
+
+main()
